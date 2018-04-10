4fb92b003c5ed5e1208dd1a0f9c30384c466a883
view <1336646875@qq.com>
2018-04-10 19:12:31 +0800

    wu

--- src/main/java/com/issc/second/dao/BlackListDao.java
+++ src/main/java/com/issc/second/dao/BlackListDao.java
@@ -2,9 +2,12 @@ package com.issc.second.dao;
 
 import com.issc.second.entity.BlackList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BlackListDao extends JpaRepository<BlackList,Long> {
 
     //根据车牌查询黑名单
     BlackList findByCardId(String CardId);
+
 }
